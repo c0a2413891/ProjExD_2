b5a58519d822cd2dc6064634ee82f35b69695068
Kei Takahashi <c0a2413891@edu.teu.ac.jp>
2025-04-22 15:20:40 +0900

    爆弾動いた

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,18 @@ DELTA = {pg.K_UP:(0,-5),
     }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound (rct:pg.Rect) -> tuple[bool,bool]:
+    """
+    引数：こうかとんrectまたは爆弾rect
+    戻り値：判定結果タプル(横、縦)
+    画面内ならTrue画面外ならFalse
+    """
+    yoko, tate = True,True #横、縦方向用の変数
+    if rct.left < 0 or WIDTH < rct.right: #画面内だったら
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko,tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -28,28 +40,40 @@ def main():
     bb_img.set_colorkey((0,0,0))
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    tmr = 0 
+    vx = 5
+    vy = -5
 
 
-    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        if kk_rct.colliderect(bb_rct): #こうかとんrectと爆弾が衝突したら
+            
+            return
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]  #左右方向
                 sum_mv[1] += mv[1]  #上下方向
-        kk_rct.move_ip(sum_mv)
+        kk_rct.move_ip(sum_mv) 
+        if check_bound(kk_rct) != (True,True): #画面外だったら
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1]) #画面内に戻す
         screen.blit(kk_img, kk_rct)
-        screen.blit(bb_img,bb_rct)
-        vx = 5
-        vy = -5
-        bb_rct.move_ip(vx,vy)
-        screen.blit(bb_img,bb_rct)
+       
+        bb_rct.move_ip(vx,vy) #爆弾の移動
+
+        yoko,tate = check_bound(bb_rct) #爆弾の画面内判定
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        screen.blit(bb_img,bb_rct) #爆弾の描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
